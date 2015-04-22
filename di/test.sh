3e5d85096996c878d3d2d23b92220aa4a9c8ed2b
Ben Schwartz <benschw@gmail.com>
2015-04-22 13:18:58 -0500

    renaming service

--- di/test.sh
+++ di/test.sh
@@ -7,7 +7,7 @@ if [ ! -f ./build/consul ]; then
 	wget -O ./build/consul.zip https://dl.bintray.com/mitchellh/consul/0.5.0_linux_amd64.zip
 	unzip ./build/consul.zip -d ./build/ 
 	chmod 755 ./build/consul
-fi
+fi 
 
 
 mkdir -p ./build/consul.d
